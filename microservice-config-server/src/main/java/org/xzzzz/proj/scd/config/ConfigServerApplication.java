fd29c8ba05686aff217a46cc32dcf73c7fe630e6
Xzzzzz <195286381@qq.com>
2020-11-08 17:52:43 +0800

    scd 整合 spring cloud config 服务

---
+++ microservice-config-server/src/main/java/org/xzzzz/proj/scd/config/ConfigServerApplication.java
@@ -0,0 +1,17 @@
+package org.xzzzz.proj.scd.config;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+/**
+ * @author xzzzz
+ * 2020/11/8
+ */
+@SpringBootApplication
+@EnableConfigServer
+public class ConfigServerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigServerApplication.class, args);
+    }
+}
